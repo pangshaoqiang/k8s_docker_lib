68d455af1bb12c734f911acc63fc18bcbb64fbaa
fenghan <fenghan>
2017-02-09 10:08:46 +0800

    bug fix

--- get_images_version.sh
+++ get_images_version.sh
@@ -4,7 +4,7 @@ images=`grep FROM -r */Dockerfile | grep gcr | awk -F '/' '{print $NF}'`
 
 echo $images
 
-\rm versions
+\rm -f versions
 touch versions
 for imageName in ${images[@]} ; do
     echo $imageName >> versions
